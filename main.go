8c0e570d5e5a055047cd64101b0a6d65d20d54a0
Ishaan <adh.ishaan10@gmail.com>
2025-07-03 22:25:10 +0530

    setup gui

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 func list_dir() []string {
@@ -26,9 +29,29 @@ func list_dir() []string {
 }
 
 func main() {
+	var data = list_dir()
 	fmt.Println("bunk")
 	a := app.New()
 	w := a.NewWindow("Encrypt UI")
-	w.SetContent(widget.NewLabel("Test"))
+	message := widget.NewLabel("Recent Files")
+	button := widget.NewButton("Decrypt", func() {
+		formatted := time.Now().Format("Time: 03:04:05")
+		message.SetText(formatted)
+	})
+	list := widget.NewList(
+		func() int {
+			return len(data)
+		},
+		func() fyne.CanvasObject {
+			return widget.NewLabel("template")
+		},
+		func(i widget.ListItemID, o fyne.CanvasObject) {
+			o.(*widget.Label).SetText(data[i])
+		})
+
+	topContainer := container.NewVBox(button, message)
+	content := container.NewBorder(topContainer, nil, nil, nil, list)
+
+	w.SetContent(content)
 	w.ShowAndRun()
 }
