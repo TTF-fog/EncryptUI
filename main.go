481cc13165a91e0ec44f034653cafbec47511d97
Ishaan Adhikari <adh.ishaan10@gmail.com>
2025-08-10 19:15:37 +0530

    better error message

--- main.go
+++ main.go
@@ -31,6 +31,7 @@ func main() {
 	message := container.NewCenter(widget.NewLabel("Recent Files"))
 	err := clipboard.Init()
 	if err != nil {
+		dialog.ShowError(errors.New("requires libx11-dev! (sudo apt-get install libx11-dev) (for clipboard functionality)"), w)
 		panic("requires libx11-dev! (sudo apt-get install libx11-dev)")
 	}
 	list := widget.NewList(
